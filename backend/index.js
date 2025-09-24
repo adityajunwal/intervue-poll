54ff6d42b7f4a4520611e82d80cd526c6a831e92
adityajunwal <adityakjunwal@gmail.com>
2025-09-24 19:11:01 +0530

    awd

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use("/poll", pollRoutes)
 
 pollSocket(io)
 
-const PORT = 3000
+const PORT = process.env.PORT ||  3000
 server.listen(PORT, () => {
     console.log(`Server running at: http://localhost:${PORT}`)
 })
